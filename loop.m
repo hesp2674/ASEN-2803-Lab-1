ee881b8369caa43d977cd7b5ac3c36fdb481d2b8
hesp2674 <hesp2674@colorado.edu>
2024-02-15 12:32:33 -0700

    Loop Deliverable

--- loop.m
+++ loop.m
@@ -6,25 +6,71 @@ close all;
 
 %Variables
 h0 = 125;
-theta = linspace (-90,270,360);
-R = 50;
+N = 750;
+theta = linspace (0,360,N);
+R = 30;
 g = 9.81;
-h = R*sind(theta) + R;
+x0 = 0;
+y0 = 0;
+z0 = 60;
 
-% G Force at different locations
+
+%Eq of Circle
+x = (R*cosd(theta)) + x0;
+z = (R*sind(theta) + R) + z0;
+y = zeros(1,length(x)) + y0;
+
+% G Force at different locations\
+v = sqrt(2*g*(h0-z));
+
 G_top = (2*(h0-2*R)/(R)) -1;
-G_angle = (2*(h0-R*sind(theta)-R)/(R))- sind(theta);
 G_bot = (2*h0/R) +1;
+G_force = ((v.^2)./(g.*R))-sind(theta);
 
-%Velocity
-V = sqrt(2*g*(h0-h));
+len_curve = sum(vecnorm(diff( [x(:),z(:)] ),2,2));
+position_vec = linspace(0, len_curve, N);
 
+%% PLOTTING
 
-% Loop diagram
-x = R*sind(theta);
-y = R*cosd(theta);
-plot3(x,y,G_angle,"Color",'b','LineWidth',2);
-xlabel('X position (m)');
-ylabel('Y position (m)');
-zlabel("G force");
+subplot(3,1,1)
+plot(position_vec, G_force)
 grid on;
+ylim([-1 6])
+xlabel('Position (m)');
+ylabel('G-Force Up/Dowm (Gs)');
+
+subplot(3,1,2)
+lat_gs = zeros(length(position_vec));
+plot(position_vec, lat_gs)
+grid on;
+ylim([-3 3])
+xlabel('Position (m)');
+ylabel('G-Force Lateral (Gs)');
+
+subplot(3,1,3)
+lat_gs = zeros(length(position_vec),1);
+plot(position_vec, lat_gs)
+grid on;
+ylim([-4 5])
+xlabel('Position (m)');
+ylabel('G-Force Forward/Back (Gs)');
+
+%G-Force Plot
+figure();
+scatter3(x,y,z,[],G_force)
+colormap;
+cb = colorbar();
+ylabel(cb,'G-Force')
+ylabel('Y Positon')
+xlabel('X Position')
+zlabel('Z Position')
+
+%Velocity Plot
+figure();
+scatter3(x,y,z,[],v)
+colormap;
+cb = colorbar();
+ylabel(cb,'Velocity')
+ylabel('Y Positon')
+xlabel('X Position')
+zlabel('Z Position')
