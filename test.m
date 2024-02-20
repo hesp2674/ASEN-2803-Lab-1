702ce8e1a746cc49c18d69ee58dd9c33338b3f22
hesp2674 <hesp2674@colorado.edu>
2024-02-20 09:57:56 -0700

    banked turn showing g plots

--- test.m
+++ test.m
@@ -2,51 +2,13 @@ clc;
 clear ;
 close all;
 
-%% LOOP 
-
-%Variables
-h0 = 125;
-theta = linspace (-90,270,1000);
-R = 50;
-g = 9.81;
-h = R*sind(theta) + R;
-
-% G Force at different locations
-G_top = (2*(h0-2*R)/(R)) -1;
-G_angle = (2*(h0-R*sind(theta)-R)/(R))- sind(theta);
-G_bot = (2*h0/R) +1;
-
-%Velocity
-V = sqrt(2*g*(h0-h));
-
-
-% Loop diagram
-x = R*sind(theta);
-y = R*cosd(theta);
+[x, y, z, position_arc_0, v, G_force, theta, angle] = ArcEnd(-150, 120, -90, 0, -5);
 
 figure();
-plot3(x,y,G_angle,"Color",'b','LineWidth',2);
-xlabel('X position (m)');
-ylabel('Y position (m)');
-zlabel("G force");
-grid on;
-
-
-figure();
-surf([x(:) x(:)], [y(:) y(:)], [G_angle(:) G_angle(:)], ...
-    'FaceColor','none', ...
-    'EdgeColor','interp', ...
-    'LineWidth', 2);
-view(2);
-xlabel('X position (m)');
-ylabel('Y position (m)');
-colorbar;
-
-
-z = R*sind(theta);
-y = R*cosd(theta);
-x=zeros(1,1000);
-figure();
-scatter3(x,y,z,[],G_angle)
+scatter3(x,y,z,[],G_force)
 colormap;
-colorbar;
+cb = colorbar();
+ylabel(cb,'G-Force')
+ylabel('Y Positon')
+xlabel('X Position')
+zlabel('Z Position')
